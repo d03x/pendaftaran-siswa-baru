b7eada00901e70a32ac6e270b7290ce26e5cca34
dadanhdyt <dadanhidyt@gmail.com>
2024-07-28 02:02:20 +0700

    use google charts

--- resources/js/dashboard/statistik-carts.js
+++ resources/js/dashboard/statistik-carts.js
@@ -1,21 +1,40 @@
-import "c3/c3.min.css"
-import c3 from "c3";
+// import "c3/c3.min.css"
+// import c3 from "c3";
 
-const chartsDuaElement = '#char2t'
+// const chartsDuaElement = '#char2t'
 
-c3.generate({
+
+
+// c3.generate({
    
-    bindto: chartsDuaElement,
-    data: {
-        type : 'pie',
-        columns: [
-            ['UMUM', 320],
-            ['PRESTASI', 520],
-            ['ZONASI', 220],
-            ['PRESTASI AKADEMIK ', 220]
-        ]
-    },
-    donut : {
-        title : "Jalur Pendaftaran"
-    }
-});
+//     bindto: chartsDuaElement,
+//     data: {
+//         type : 'pie',
+//         columns: [
+//             ['UMUM', 320],
+//             ['PRESTASI', 520],
+//             ['ZONASI', 220],
+//             ['PRESTASI AKADEMIK ', 220]
+//         ]
+//     },
+//     donut : {
+//         title : "Jalur Pendaftaran"
+//     }
+// });
+import {GoogleCharts} from 'google-charts';
+
+
+function drawChart() {
+
+    const data = GoogleCharts.api.visualization.arrayToDataTable([
+        ['Pendaftar', 'Total Pendaftar'],
+        ['UMUM', 620],
+        ['PRESTASI', 122],
+        ['ZONASI', 182]
+    ]);
+    const cartStatistikJalur = new GoogleCharts.api.visualization.ColumnChart(document.getElementById('char2t'));
+    cartStatistikJalur.draw(data,{
+        title : "Total Pendaftar Berdasarkan Jalur",
+    });
+}
+GoogleCharts.load(drawChart);
