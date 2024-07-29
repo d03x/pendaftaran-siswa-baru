ec5306bf533f9ce79b1a58efeff33dcc0368a0e2
dadanhdyt <dadanhidyt@gmail.com>
2024-07-29 13:59:39 +0700

    refactor:full refactor form review

--- resources/js/utils/components/formReview.js
+++ resources/js/utils/components/formReview.js
@@ -1,63 +1,65 @@
 import { parseInputName } from "../../utils";
+
 /**
- * 
- * @param {*} name 
- * @param {*} value 
- * @returns 
+ * Generates a table row for the given input name and value.
+ *
+ * @param {string} name - The name of the input field.
+ * @param {string} value - The value of the input field.
+ * @returns {string} - The HTML string for the table row.
  */
 function generateTableRow(name, value) {
+    const displayValue = value 
+        ? value 
+        : '<i class="tw-text-red-500 text-decoration-underline tw-text-sm">Tolong Isi Dulu</i>';
+    
     return `
-               <tr>
-                  <td>${parseInputName(name)}</td>
-                  <td class="tw-px-3">:</td>
-                  <td>${value
-            ? value
-            : '<i class="tw-text-red-500 text-decoration-underline tw-text-sm">Tolong Isi Dulu</i>'
-        }</td>
-                </tr> 
-              `;
+        <tr>
+            <td>${parseInputName(name)}</td>
+            <td class="tw-px-3">:</td>
+            <td>${displayValue}</td>
+        </tr>
+    `;
 }
 
 /**
+ * Generates an HTML table for form review, excluding specified fields.
  *
- * @param {*} inputs
- * @param {*} excepts
- * @returns
+ * @param {HTMLElement} formElement - The form element to review.
+ * @param {Array<string>} excepts - The input names to exclude from the review.
+ * @returns {string} - The HTML string for the review table.
  */
-const generateReviewTable = (formElement, excepts) => {
+function generateReviewTable(formElement, excepts) {
     const inputs = formElement.querySelectorAll("input, select, textarea");
-
     let data = "<table>";
+    
     inputs.forEach((input) => {
         if (!excepts.includes(input.name)) {
             data += generateTableRow(input.name, input.value);
-
         }
     });
-    
+
     data += "</table>";
     return data;
-};
+}
+
 /**
+ * Generates a form review and inserts it into the specified element, or returns the review HTML.
  *
- * @param {*} selector
- * @param {*} excepts
- * @param {*} config
- * @returns
+ * @param {string} selector - The selector for the form element to review.
+ * @param {Array<string>} excepts - The input names to exclude from the review.
+ * @param {Object} config - Configuration object for review placement.
+ * @param {string} config.modalContentSelector - The selector for the element to insert the review into.
+ * @returns {string|void} - The HTML string for the review table or void if inserted into a modal.
  */
 function formReview(selector, excepts, config) {
     const formElement = document.querySelector(selector);
+    const reviewContentElement = document.querySelector(config.modalContentSelector);
+    const reviewHTML = generateReviewTable(formElement, excepts);
 
-    const reviewContentElement = document.querySelector(
-        config.modalContentSelector
-    );
     if (reviewContentElement) {
-        reviewContentElement.innerHTML = generateReviewTable(
-            formElement,
-            excepts
-        );
+        reviewContentElement.innerHTML = reviewHTML;
     } else {
-        return generateReviewTable(formElement, excepts);
+        return reviewHTML;
     }
 }
 
